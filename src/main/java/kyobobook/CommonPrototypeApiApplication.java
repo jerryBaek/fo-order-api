969f06450a2a42b28ae28f520fd905340de589f1
leesangmo <smlee1@kyobobook.com>
2021-08-10 09:43:54 +0900

    init commit

---
+++ src/main/java/kyobobook/CommonPrototypeApiApplication.java
@@ -0,0 +1,13 @@
+package kyobobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonPrototypeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonPrototypeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kyobobook/CommonPrototypeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kyobobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonPrototypeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
