67bed4aede4f9b0a08463698bb2929614ba2a768
ldcc_kimsehoon <kimsehoon@kyobobook.com>
2021-11-23 16:57:50 +0900

    상수 객체 추가

---
+++ src/main/java/kyobobook/common/Constants.java
@@ -0,0 +1,38 @@
+/***************************************************
+ * Copyright(c) 2021-2022 Kyobo Book Centre All right reserved.
+ * This software is the proprietary information of Kyobo Book.
+ *
+ * Revision History
+ * Author                         Date          Description
+ * --------------------------     ----------    ----------------------------------------
+ * kimsehoon@kyobobook.com      2021. 11. 23.
+ *
+ ****************************************************/
+package kyobobook.common;
+
+/**
+ * @Project : fo-order-api
+ * @FileName : Constants.java
+ * @Date : 2021. 11. 23.
+ * @author : kimsehoon@kyobobook.com
+ * @description :
+ */
+public class Constants {
+
+    /**
+     * @Project : fo-order-api
+     * @FileName : Constants.java
+     * @Date : 2021. 11. 23.
+     * @author : kimsehoon@kyobobook.com
+     * @description : 메시지 소스 상수
+     */
+    public static class MessageSource {
+
+        /** 완료 */
+        public static final String COMPLETE = "common.process.complete";
+        /** 오류 */
+        public static final String ERROR = "common.process.error";
+
+    }
+
+}
