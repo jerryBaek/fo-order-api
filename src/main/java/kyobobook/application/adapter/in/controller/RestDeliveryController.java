86349e2dfb0ff02a10d4cbdf3999ca9aca265fa5
koseohee <seohee.ko@kyobobook.com>
2021-11-24 14:44:05 +0900

    RestDeliveryController 생성

---
+++ src/main/java/kyobobook/application/adapter/in/controller/RestDeliveryController.java
@@ -0,0 +1,22 @@
+/***************************************************
+ * Copyright(c) 2021-2022 Kyobo Book Centre All right reserved.
+ * This software is the proprietary information of Kyobo Book.
+ *
+ * Revision History
+ * Author                         Date          Description
+ * --------------------------     ----------    ----------------------------------------
+ * seohee.ko@kyobobook.com      2021. 11. 24.
+ *
+ ****************************************************/
+package kyobobook.application.adapter.in.controller;
+
+/**
+ * @Project     : fo-order-api
+ * @FileName    : RestDeliveryController.java
+ * @Date        : 2021. 11. 24.
+ * @author      : seohee.ko@kyobobook.com
+ * @description :
+ */
+public class RestDeliveryController {
+
+}
